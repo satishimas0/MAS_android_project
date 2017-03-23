f93596b57dbc2c52193cc711bf201a5dafa4dd82
satishimas0 <satishimas0@gmail.com>
2017-03-23 11:21:54 +0530

    test
    test project created

---
+++ MASandroidproject/app/src/main/java/traindge/masandroidproject/feedback_activity.java
@@ -0,0 +1,13 @@
+package traindge.masandroidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class feedback_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
