44a1e5f47702afdb1b82c35f37227abc810df8c4
ISHANCHAURASIA <ishanchaurasia199426@gmail.com>
2017-04-14 09:58:09 +0530

    feedback
    feedbackchanges

--- MASandroidproject/app/src/main/java/traindge/masandroidproject/FeedbackActivity.java
+++ MASandroidproject/app/src/main/java/traindge/masandroidproject/FeedbackActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class FeedbackActivity extends AppCompatActivity {
 
     private Button btn_feedback_send;
@@ -13,9 +14,14 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feddback);
-        EditText et_subjectfeedback=(EditText)findViewById(R.id.et_subjectfeedback);
+
         EditText tvfeedbackMsg=(EditText)findViewById(R.id.tvfeedbackMsg);
         btn_feedback_send = (Button) findViewById(R.id.btn_feedback_send);
         //create onclicklistener
+
+        // our code
+        EditText et_subjectfeedback=findViewById(R.id.et_subjectfeedback);
+
     }
+    EditText et_subjectfeedback=findViewById(R.id.et_subjectfeedback);
 }
