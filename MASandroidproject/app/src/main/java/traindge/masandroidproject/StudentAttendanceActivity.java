a4303a911c21abcf03ff0fe4d6e07b314169f5d8
satishimas0 <satishimas0@gmail.com>
2017-04-25 12:53:01 +0530

    by satish
    attendance

--- MASandroidproject/app/src/main/java/traindge/masandroidproject/StudentAttendanceActivity.java
+++ MASandroidproject/app/src/main/java/traindge/masandroidproject/StudentAttendanceActivity.java
@@ -2,15 +2,12 @@ package traindge.masandroidproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class StudentAttendance extends AppCompatActivity {
+public class StudentAttendanceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +27,17 @@ public class StudentAttendance extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.Report:
-                Intent IntentHome = new Intent(StudentAttendance.this, Report.class);
+                Intent IntentHome = new Intent(StudentAttendanceActivity.this, Report.class);
                 startActivity(IntentHome);
                 break;
             case R.id.Notice:
-                Intent IntentHom = new Intent(StudentAttendance.this, NoticeSystemActivity.class);
+                Intent IntentHom = new Intent(StudentAttendanceActivity.this, NoticeSystemActivity.class);
                 startActivity(IntentHom);
                 break;
             case R.id.about:
                 break;
             case R.id.feedback:
-                Intent IntentH = new Intent(StudentAttendance.this, FeedbackActivity.class);
+                Intent IntentH = new Intent(StudentAttendanceActivity.this, FeedbackActivity.class);
                 startActivity(IntentH);
                 break;
         }
